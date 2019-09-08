1fbd5fdf1a5791d352ef8940eb41b30f0d8eb22a
Pieter Droogendijk <pieter@binky.org.uk>
2019-09-08 15:07:44 +0200

    Added shorthand

---
+++ shorthand/decoder.go
@@ -0,0 +1,141 @@
+package shorthand
+
+import (
+	"encoding/binary"
+	"fmt"
+	"hash/crc32"
+
+	"github.com/PieterD/debbie/panic"
+	"github.com/pkg/errors"
+)
+
+const (
+	maxUint = uint64(^uint(0))
+	maxInt  = int64(int(maxUint >> 1))
+	minInt  = int64(-maxInt - 1)
+)
+
+var (
+	ErrInvalidCRC = fmt.Errorf("invalid CRC")
+)
+
+func Recover(errp *error) {
+	if err := panic.RecoverCheck(recover()); err != nil {
+		*errp = err
+	}
+}
+
+type Decoder struct {
+	buf    []byte
+	pos    int
+	crcPos int
+}
+
+func NewDecoder(b []byte) *Decoder {
+	return &Decoder{
+		buf: b,
+	}
+}
+
+func (d *Decoder) Len() int {
+	return len(d.buf) - d.pos
+}
+
+func (d *Decoder) Buffer() []byte {
+	return d.buf[d.pos:]
+}
+
+func (d *Decoder) Available(fun, field string, num int) []byte {
+	if d.pos+num > len(d.buf) {
+		panic.Panic(errors.Errorf("%s(%s) not enough bytes, needed %d", fun, field, num))
+	}
+	return d.buf[d.pos : d.pos+num]
+}
+
+func (d *Decoder) Advance(fun, field string, n int) {
+	d.Available(fun, field, n)
+	d.pos += n
+}
+
+func (d *Decoder) Uint8(field string) uint8 {
+	b := d.Available("Uint8", field, 1)
+	i := b[0]
+	d.Advance("Uint8", field, 1)
+	return i
+}
+
+func (d *Decoder) Uint16(field string) uint16 {
+	b := d.Available("Uint16", field, 2)
+	i := binary.BigEndian.Uint16(b)
+	d.Advance("Uint16", field, 2)
+	return i
+}
+
+func (d *Decoder) Uint32(field string) uint32 {
+	b := d.Available("Uint32", field, 4)
+	i := binary.BigEndian.Uint32(b)
+	d.Advance("Uint32", field, 4)
+	return i
+}
+
+func (d *Decoder) Uint64(field string) uint64 {
+	b := d.Available("Uint64", field, 8)
+	i := binary.BigEndian.Uint64(b)
+	d.Advance("Uint64", field, 8)
+	return i
+}
+
+func (d *Decoder) VarInt(field string) int {
+	i := d.VarInt64(field)
+	if i > maxInt {
+		panic.Panic(errors.Errorf("VarInt(%s) %d too large (>%d) for int", field, i, maxInt))
+	} else if i < minInt {
+		panic.Panic(errors.Errorf("VarInt(%s) %d too small (<%d) for int", field, i, minInt))
+	}
+	return int(i)
+}
+
+func (d *Decoder) VarInt64(field string) int64 {
+	i, n := binary.Varint(d.Buffer())
+	if n == 0 {
+		panic.Panic(errors.Errorf("VarInt64(%s) buffer too small", field))
+	}
+	if n < 0 {
+		panic.Panic(errors.Errorf("VarInt64(%s) overflow(%d)", field, n))
+	}
+	d.Advance("VarInt64", field, n)
+	return i
+}
+
+func (d *Decoder) VarUint64(field string) uint64 {
+	i, n := binary.Uvarint(d.Buffer())
+	if n == 0 {
+		panic.Panic(errors.Errorf("VarInt64(%s) buffer too small", field))
+	}
+	if n < 0 {
+		panic.Panic(errors.Errorf("VarInt64(%s) overflow(%d)", field, n))
+	}
+	d.Advance("VarUint64", field, n)
+	return i
+}
+
+func (d *Decoder) Bytes(field string, num int) []byte {
+	b := d.Available("Bytes", field, num)
+	cop := make([]byte, num)
+	copy(cop, b)
+	d.Advance("Bytes", field, num)
+	return b
+}
+
+func (d *Decoder) StartCRC() {
+	d.crcPos = d.pos
+}
+
+func (d *Decoder) CheckCRC(field string) {
+	_ = d.Available("CheckCRC", field, 4)
+	got := crc32.ChecksumIEEE(d.buf[d.crcPos:d.pos])
+	want := d.Uint32(field)
+	if want != got {
+		panic.Panic(errors.Wrapf(ErrInvalidCRC, "CheckCRC(%s) invalid crc", field))
+	}
+}
---
+++ shorthand/encoder.go
@@ -0,0 +1,88 @@
+package shorthand
+
+import (
+	"encoding/binary"
+	"hash/crc32"
+	"sync"
+)
+
+type Encoder struct {
+	b      []byte
+	crcPos int
+}
+
+var bytesPool = sync.Pool{New: func() interface{} { return make([]byte, 1024) }}
+
+func NewEncoder() *Encoder {
+	return &Encoder{
+		b: bytesPool.Get().([]byte)[:0],
+	}
+}
+
+func (e *Encoder) Close() {
+	bytesPool.Put(e.b)
+}
+
+func (e *Encoder) Advance(size int) {
+	e.b = e.b[:len(e.b)+size]
+}
+
+func (e *Encoder) Grow(required int) []byte {
+	if cap(e.b) < len(e.b)+required {
+		e.b = append(e.b, make([]byte, required)...)
+	}
+	return e.b[len(e.b) : len(e.b)+required]
+}
+
+func (e *Encoder) Copy() []byte {
+	c := make([]byte, len(e.b), len(e.b))
+	copy(c, e.b)
+	return c
+}
+
+func (e *Encoder) Buffer() []byte {
+	return e.b
+}
+
+func (e *Encoder) Uint8(i uint8) {
+	e.b = append(e.b, i)
+}
+
+func (e *Encoder) Uint16(i uint16) {
+	binary.BigEndian.PutUint16(e.Grow(2), i)
+	e.Advance(2)
+}
+
+func (e *Encoder) Uint32(i uint32) {
+	binary.BigEndian.PutUint32(e.Grow(4), i)
+	e.Advance(4)
+}
+
+func (e *Encoder) Uint64(i uint64) {
+	binary.BigEndian.PutUint64(e.Grow(8), i)
+	e.Advance(8)
+}
+
+func (e *Encoder) VarInt(i int) {
+	e.VarInt64(int64(i))
+}
+
+func (e *Encoder) VarInt64(i int64) {
+	e.Advance(binary.PutVarint(e.Grow(binary.MaxVarintLen64), i))
+}
+
+func (e *Encoder) VarUint64(u uint64) {
+	e.Advance(binary.PutUvarint(e.Grow(binary.MaxVarintLen64), u))
+}
+
+func (e *Encoder) Bytes(b []byte) {
+	e.b = append(e.b, b...)
+}
+
+func (e *Encoder) StartCRC() {
+	e.crcPos = len(e.b)
+}
+
+func (e *Encoder) PutCRC() {
+	e.Uint32(crc32.ChecksumIEEE(e.b[e.crcPos:]))
+}
